7121a2b0b209d41fa49545c8daa9d52613080edb
teru12012000 <sion0012@outlook.jp>
2022-07-27 16:35:54 +0900

    reactのfullcalendar最強じゃね？

---
+++ calendar/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Calendar from './components/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form">
+        <Calendar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
